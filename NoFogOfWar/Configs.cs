542d7dcec441feaf9ffa1fb70a76e8ffd6afb65f
MitraX <mitrax215@gmail.com>
2025-12-06 19:34:37 +0300

    feat: Allows the player to explore all the settlements

--- NoFogOfWar/Configs.cs
+++ NoFogOfWar/Configs.cs
@@ -19,5 +19,9 @@ namespace NoFogOfWar
         [SettingPropertyBool(displayName: "{=NoFogOfWar.4f8e20f7}Heroes", Order = 0, RequireRestart = false, HintText = "{=NoFogOfWar.6ffb53b3}It allows the player to get to know all the heroes.")]
         [SettingPropertyGroup(HeadingNoFogOfWar)]
         public bool Heroes { get; set; } = true;
+
+        [SettingPropertyBool(displayName: "{=NoFogOfWar.6d91efcd}Settlements", Order = 1, RequireRestart = false, HintText = "{=NoFogOfWar.42c2da62}It allows the player to explore all the settlements.")]
+        [SettingPropertyGroup(HeadingNoFogOfWar)]
+        public bool Settlements { get; set; } = true;
     }
 }
---
+++ NoFogOfWar/Patches/DefaultInformationRestrictionModelPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.CampaignSystem.GameComponents;
+
+namespace NoFogOfWar.Patches
+{
+    [HarmonyPatch(typeof(DefaultInformationRestrictionModel))]
+    class DefaultInformationRestrictionModelPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("DoesPlayerKnowDetailsOf", new System.Type[] { typeof(Settlement) })]
+        public static bool DoesPlayerKnowDetailsOf(ref bool __result)
+        {
+            if (!Configs.Instance.Settlements)
+            {
+                return true;
+            }
+
+            __result = true;
+
+            return false;
+        }
+    }
+}
